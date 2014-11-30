bdf7547e1a36dbbad6aa50e633e950fbbed00a32
Arfurboy <Arfurboy@gmail.com>
2014-11-29 18:17:27 -0800

    Partial mongo db refactor

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,26 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongoose.Schema({
+  link: String,
+  code: String,
+  visits: Number,
+  title: String,
+  base_url: String,
+  url: String
 });
+
+var shaHash = function(url){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0,5);
+};
+
+linkSchema.pre('save', function(next){
+  this.code = shaHash(this.url);
+  next();
+})
+
+var Link = mongoose.model('Link', linkSchema);
 
 module.exports = Link;
